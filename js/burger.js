27efabca79dc8a76cbfbb50544f458491fbfc0ba
noah <ehson.ehsonov@gmail.com>
2024-03-27 19:49:20 +0300

    Добавил прослушку событий на бургер и анимацию

---
+++ js/burger.js
@@ -0,0 +1,11 @@
+const headerBurger = document.querySelector('.header__burger');
+const headerNav = document.querySelector('.header__nav');
+const headerNavClose = document.querySelector('.header__nav-close');
+
+headerBurger.addEventListener('click', () => {
+    headerNav.classList.toggle('d__block');
+})
+
+headerNavClose.addEventListener('click', () => {
+    headerNav.classList.toggle('d__block');
+})
